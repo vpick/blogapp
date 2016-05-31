0ea1c23a1f633a0f68608a210b21e114c7fbd72c
vpick <varsha123verma@gmail.com>
2016-05-31 18:45:46 +0530

    All files are modified

--- blogapp/blog/views.py
+++ blogapp/blog/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 def post_list(request):
      posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
      return render(request, 'blog/post_list.html', {'posts':  posts})
-from django.shortcuts import render, get_object_or_404
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
-from .forms import PostForm
+
+
 def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -25,4 +31,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        return render(request, 'blog/post_edit.html', {'form': form})
